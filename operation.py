c56bfdc2b3c39fbb5203924659a44c66582ce18e
fernando-carvalho <fernandocarvalho3101@gmail.com>
2016-09-14 14:19:28 -0300

    Update operation.py to work as a transpiler

--- operation.py
+++ operation.py
@@ -1,20 +1,38 @@
 
 class DataReader():
-    def __init__(self,infile):
-        self.infile = infile
-    def read_csv(self, has_header, sep_char, spark):
-        return spark.read.csv(self.infile, header=has_header, sep=sep_char)
+    def __init__(self, parameters, input_df, output_df):
+        self.infile = parameters['infile']
+        self.header = parameters['has_header']
+        self.delimiter = parameters['sep']
+        self.input_df = input_df
+        self.output_df = output_df
+    def generate_code(self):
+        spark_code = (self.output_df[0] + " = spark.read.csv('" + self.infile + "', header=" + self.header + ", sep='" + self.delimiter + "')")
+        return spark_code
 
 
 class RandomSplit():
+    def __init__(self, parameters, input_df, output_df):
+        self.weight_1 = parameters['weights'][0]
+        self.weight_2 = parameters['weights'][1]
+        self.seed = parameters['seed']
+        self.input_df = input_df
+        self.output_df = output_df
+    def generate_code(self):
+        spark_code = (self.output_df[0] + " = " + self.input_df[0] + ".randomSplit([" + self.weight_1 + \
+                     ", " + self.weight_2 + "], " + self.seed + ")")
+        spark_code += "\n" + self.output_df[1] + " = " + self.output_df[0] + "[1]"
+        spark_code += "\n" + self.output_df[0] + " = " + self.output_df[0] + "[0]"
+        return spark_code
 
-    def __init__(self, weights, seed):
-        self.weight_1 = weights[0]
-        self.weight_2 = weights[1]
-        self.seed = seed
- 
-    def split(self, dataframe):
-        return dataframe.randomSplit([self.weight_1,self.weight_2], self.seed)
 
-
-
+class Union():
+    def __init__(self, parameters, input_df, output_df):
+        self.distinct = parameters['distinct']
+        self.input_df = input_df
+        self.output_df = output_df
+    def generate_code(self):
+        spark_code = (self.output_df[0] + " = " + self.input_df[0] + ".unionAll(" + self.input_df[1] + ")")
+        if (self.distinct == "True"):
+           spark_code += ".distinct()"
+        return spark_code
