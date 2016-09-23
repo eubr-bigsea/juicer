21539b4611d11d7b1d350974f3b8ed28bc5ad556
Ubuntu <ubuntu@spark01.ctweb.inweb.org.br>
2016-09-23 11:56:52 -0300

    More operations added

--- juicer/spark/operation.py
+++ juicer/spark/operation.py
@@ -1,6 +1,16 @@
+from textwrap import dedent
 import json
 
-class DataReader():
+
+
+class operation():
+    def set_io(self, inputs, outputs):
+        self.inputs = inputs
+        self.outputs = outputs
+
+
+
+class DataReader(operation):
     '''
     Reads a database.
     Parameters:
@@ -8,74 +18,163 @@ class DataReader():
     - Boolean value indicating if the file has a header.
     - Delimiter char for the csv file.
     '''
-    def __init__(self, parameters, input_df, output_df):
+    def __init__(self, parameters, inputs, outputs):
         self.infile = parameters['infile']
         self.header = parameters['has_header']
         self.delimiter = parameters['sep']
-        self.input_df = input_df
-        self.output_df = output_df
+        self.set_io(inputs, outputs)
     def generate_code(self):
-        spark_code = (self.output_df[0] + " = spark.read.csv('" + self.infile + \
-                      "', header=" + self.header + ", sep='" + self.delimiter + "')")
-        return spark_code
+        code = """{0} = spark.read.csv('{1}', header={2}, sep='{3}')""".format(
+            self.outputs[0], self.infile, self.header, self.delimiter)
+        return dedent(code)
 
 
 
-class RandomSplit():
+class RandomSplit(operation):
     '''
     Randomly splits the Data Frame into two data frames.
     Parameters:
     - List with two weights for thw two new data frames.
     - Optional seed in case of deterministic random operation ('0' means no seed).
     '''
-    def __init__(self, parameters, input_df, output_df):
-        self.weight_1 = parameters['weights'][0]
-        self.weight_2 = parameters['weights'][1]
+    def __init__(self, parameters, inputs, outputs):
+        self.weights = map(lambda x: float(x), parameters['weights'])
         self.seed = parameters['seed']
-        self.input_df = input_df
-        self.output_df = output_df
+        self.set_io(inputs, outputs)
     def generate_code(self):
-        spark_code = (self.output_df[0] + " = " + self.input_df[0] + ".randomSplit([" + self.weight_1 + \
-                     ", " + self.weight_2 + "], " + self.seed + ")")
-        spark_code += "\n" + self.output_df[1] + " = " + self.output_df[0] + "[1]"
-        spark_code += "\n" + self.output_df[0] + " = " + self.output_df[0] + "[0]"
-        return spark_code
+        code = """{0}, {1} = {2}.randomSplit({3}, {4})""".format(
+            self.outputs[0], self.outputs[1], self.inputs[0],
+            json.dumps(self.weights), self.seed)
+        return dedent(code)
 
 
 
-class Union():
+class Union(operation):
     '''
     Return a new DataFrame containing union of rows in this frame and another frame.
     Parameter: boolean distinct indicating if duplicates should be removed.
     '''
-    def __init__(self, parameters, input_df, output_df):
+    def __init__(self, parameters, inputs, outputs):
         self.distinct = parameters['distinct']
-        self.input_df = input_df
-        self.output_df = output_df
+        self.set_io(inputs, outputs)
     def generate_code(self):
-        spark_code = (self.output_df[0] + " = " + self.input_df[0] + ".unionAll(" + self.input_df[1] + ")")
+        code = "{0} = {1}.unionAll({2})".format(self.outputs[0], 
+            self.inputs[0], self.inputs[1])
         if (self.distinct == "True"):
-           spark_code += ".distinct()"
-        return spark_code
+           code += ".distinct()"
+        else:
+           code + "\n"
+        return dedent(code)
 
 
 
 
-class Sort():
+class Sort(operation):
     ''' 
     Returns a new DataFrame sorted by the specified column(s).
     Parameters:
     - The list of columns to be sorted.
     - A list indicating whether the sort order is ascending for the columns.
-    Condition: the list of columns should have the same size of the list of boolean to indicating if it is ascending sorting.
+    Condition: the list of columns should have the same size of the list of 
+               boolean to indicating if it is ascending sorting.
     '''
-    def __init__(self, parameters, input_df, output_df):
+    def __init__(self, parameters, inputs, outputs):
         self.columns = parameters['columns']
-        self.ascending = parameters['ascending']
-        self.input_df = input_df
-        self.output_df = output_df
+        self.ascending = map(lambda x: int(x), parameters['ascending'])
+        self.set_io(inputs, outputs)
     def generate_code(self):
-        spark_code = (self.output_df[0] + " = " + self.input_df[0] + ".orderBy(" + str(json.dumps(self.columns)) + \
-                     ", ascending = " + str(json.dumps(self.ascending)) + ")")
-        return spark_code
+        code = "{0} = {1}.orderBy({2}, ascending={3})".format(self.outputs[0], 
+        self.inputs[0], str(json.dumps(self.columns)), str(json.dumps(self.ascending)))
+        return dedent(code)
+
+
+
+# FALTA TESTAR
+class Distinct(operation):
+    '''
+    Returns a new DataFrame containing the distinct rows in this DataFrame.
+    No parameters required.
+    '''
+    def __init__(self, parameters, inputs, outputs):
+       self.set_io(inputs, outputs) 
+    def generate_code(self):
+        code = "{} = {}.distinct()".format(
+            self.outputs[0], self.inputs[0])
+        return dedent(code)
+
+
+
+# FALTA TESTAR
+class Sample(operation):
+    '''
+    Returns a sampled subset of this DataFrame.
+    Parameters:
+    - withReplacement -> can elements be sampled multiple times (replaced when sampled out)
+    - fraction -> fraction of the data frame to be sampled.
+        without replacement: probability that each element is chosen; 
+            fraction must be [0, 1]
+        with replacement: expected number of times each element is chosen; 
+            fraction must be >= 0
+    - seed -> seed for random operation.
+    '''
+    def __init__(self, parameters, inputs, outputs):
+        self.withReplacement = parameters['withReplacement']
+        self.fraction = parameters['fraction']
+        self.seed = parameters['seed']
+        self.set_io(inputs, outputs)
+    def generate_code(self):
+        code = "{} = {}.sample(withReplacement={}, fraction={}, seed={})".format(
+            self.outputs[0], self.inputs[0], self.withReplacement, 
+            self.fraction, self.seed)
+        return dedent(code)
+
+
+
+# FALTA TESTAR
+class Save(operation):
+    '''
+    Saves the content of the DataFrame at the specified path.
+    Parameters:
+    - Format -> Supported formats: CSV, Json and Parquet.
+      CSV files must have header, comma as separator and quotation marks for strings
+    - Path -> the path in any Hadoop supported file system
+    - Mode -> specifies the behavior of the save operation when data already exists.
+              (append or overwrite or ignore or error)
+    - Compression ->  compression codec to use when saving to file.
+                      (none, bzip2, gzip, lz4, snappy and deflate)   
+    '''
+    def __init__(self, parameters, inputs, outputs):
+        self.path = parameters['path']
+        self.file_format = parameters['format']
+        self.mode = parameters['mode']
+#        if parameters.has_key['mode']:
+#            self.mode = parameters['mode']
+#        else:
+#            self.mode = 'error'
+#        if parameters.has_key('compression'):
+#            self.compression = parameters['compression']
+#        else:
+#            self.compression = None
+        self.set_io(inputs, outputs)
+    def generate_code(self):
+        code = "{}.write.format('{}').mode('{}').save('{}')".format(
+            self.inputs[0], self.file_format, self.mode, self.path)
+#        code = "{}.write.csv('{}')".format(
+#            self.inputs[0], self.path)
+        return dedent(code)
+
+
+
+
+
+#class (operation):
+#    def __init__(self, parameters, inputs, outputs):
+#        self. = parameters['']
+#        self. = parameters['']
+#        self. = parameters['']
+#    def generate_code(self):
+#        code = "{}.({}, {}, {})".format(
+#            self.inputs[0], self.file_format, self.mode, self.path)
+#        return dedent(code)
+
 
