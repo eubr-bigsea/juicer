5c6a6ea3051a5a32e95d92069ba8454a397a7bfd
Walter dos Santos Filho <waltersf@gmail.com>
2018-03-14 19:11:38 -0300

    Now files define string literals as unicode.
    In order to avoid conflics in constants and variables.

--- juicer/spark/geo_operation.py
+++ juicer/spark/geo_operation.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 import json
 from itertools import izip_longest
 from textwrap import dedent
--- juicer/spark/ml_operation.py
+++ juicer/spark/ml_operation.py
@@ -1,4 +1,6 @@
 # coding=utf-8
+from __future__ import unicode_literals
+
 import json
 import logging
 from itertools import izip_longest
