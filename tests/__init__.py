bcf2046c5378ab39cc7f9734b3cc141b84c12eb0
Walter dos Santos Filho <waltersf@gmail.com>
2018-08-14 10:55:37 -0300

    Fix clustering model and its tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,17 +1,29 @@
 # -*- coding: utf-8 -*-
 import ast
-import os
 import gettext
 
+try:
+    from itertools import zip_longest
+except ImportError:
+    from itertools import izip_longest as zip_longest
+
+import os
 
 locales_path = os.path.join(os.path.dirname(__file__), 'i18n', 'locales')
 t = gettext.translation('messages', locales_path, ["pt"],
-	fallback=True)
+                        fallback=True)
 t.install()
 
 
 def format_code_comparison(node1, node2):
-    return '\n>>>>>\n{}\n>>>>>\n{}\n-----'.format(node1, node2)
+    lines_left = node1.split('\n')
+    lines_right = node2.split('\n')
+
+    code = ['{:<4} {:<85} | {}'.format(i + 1, l[:81] if l else '',
+                                       r[:81] if r else '')
+            for i, (l, r) in enumerate(zip_longest(lines_left, lines_right))]
+    # return '\n>>>>>\n{}\n>>>>>\n{}\n-----'.format(node1, node2)
+    return '\n' + '\n'.join(code)
 
 
 current_line = current_offset = 0
@@ -28,7 +40,7 @@ def compare_ast(node1, node2):
                 current_line = vars(node2)['lineno']
             elif kind == 'col_offset':
                 current_offset = vars(node2)['col_offset']
-            elif kind not in ('ctx', ):
+            elif kind not in ('ctx',):
                 var2 = vars(node2).get(kind)
                 result, msg = compare_ast(var, var2)
                 if not result:
