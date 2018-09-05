5a8b437635a29bd329d746391e87034b72952b87
Walter dos Santos Filho <waltersf@gmail.com>
2018-09-05 16:48:57 -0300

    Implementation of reading and writing data in Sklearn

--- juicer/sklearn/data_operation.py
+++ juicer/sklearn/data_operation.py
@@ -1,9 +1,11 @@
 # coding=utf-8
-import decimal
+import json
+import uuid
 from textwrap import dedent
 
 import datetime
 
+from jinja2 import Environment, BaseLoader
 from juicer.operation import Operation
 from juicer.service import limonero_service
 
@@ -33,6 +35,20 @@ class DataReaderOperation(Operation):
         '{new_line}': '\\n',
     }
 
+    SUPPORTED_DRIVERS = {
+        'mysql': dedent("""
+            import pymysql
+            query = '{table}'
+            connection = pymysql.connect(
+                host='{host}',
+                port={port},
+                user='{user}',
+                password='{password}',
+                db='{db}',
+                charset='utf8')
+            {out} = pd.read_sql(query, con=connection)
+        """)
+    }
     LIMONERO_TO_PANDAS_DATA_TYPES = {
         "CHARACTER": 'object',
         "DATETIME": 'object',
@@ -130,8 +146,8 @@ class DataReaderOperation(Operation):
                         for attr in attrs:
                             if attr['type'] == 'DATETIME':
                                 parse_dates.append(attr['name'])
-                            elif attr['type'] == 'DECIMAL':
-                                converters[attr['name']] = 'decimal.Decimal'
+                            # elif attr['type'] == 'DECIMAL':
+                            #    converters[attr['name']] = 'decimal.Decimal'
                             self._add_attribute_to_schema(attr, code)
                     else:
                         code.append("columns['value'] = '11'")
@@ -232,77 +248,25 @@ class DataReaderOperation(Operation):
 
         return '\n'.join(code)
 
-        # url = self.name_file
-        # parsed = urlparse(url)
-        # if parsed.scheme == 'hdfs':
-        #     code = """
-        #     import pyarrow as pa
-        #
-        #     fs = pa.hdfs.connect('{hdfs_server}', {hdfs_port})
-        #     header = {header}
-        #     with fs.open('{path}', 'rb') as f:
-        #         {output} = pd.read_csv(
-        #             f, sep='{sep}', encoding='utf-8', header=header,
-        #             na_values={na_values})
-        #         if header == 0:
-        #             {output}.columns = ['col_{{col}}'.format(col=col)
-        #                 for col in {output}.columns]
-        #     """.format(path=parsed.path,
-        #                output=self.output,
-        #                hdfs_server=parsed.hostname,
-        #                hdfs_port=parsed.port,
-        #                input=parsed.path,
-        #                sep=self.separator,
-        #                header=1 if self.header else 0,
-        #                na_values=self.null_values if len(
-        #                    self.null_values) else 'None')
-        # else:
-        #
-        #     code = "{output} = pd.read_csv('{input}', sep='{sep}', " \
-        #            "encoding='utf-8'".format(output=self.output,
-        #                                      input=self.name_file,
-        #                                      sep=self.separator)
-        #
-        #     if not self.header:
-        #         code += ", header=None"
-        #
-        #     if len(self.null_values) > 0:
-        #         code += ", na_values= {}".format(self.null_values)
-        #     code += ")\n"
-        #
-        #     if not self.header:
-        #         code += "{out}.columns = ['col_'+str(col) " \
-        #                 "for col in {out}.columns]".format(out=self.output)
-        # return dedent(code)
-
     def _generate_code_for_jdbc(self, code):
 
         parsed = urlparse(self.metadata['url'])
         qs_parsed = parse_qs(parsed.query)
-        driver = self.SUPPORTED_DRIVERS.get(parsed.scheme)
-        if driver is None:
+        if parsed.scheme not in self.SUPPORTED_DRIVERS:
             raise ValueError(
                 _('Database {} not supported').format(parsed.scheme))
         if not self.metadata.get('command'):
             raise ValueError(
                 _('No command nor table specified for data source.'))
-        code_jdbc = dedent("""
-            query = '{table}'
-            if query.strip()[:6].upper() == 'SELECT':
-                # Add parentheses required by Spark
-                query = '(' + query + ') AS tb'
-            {out} = spark_session.read.format('jdbc').load(
-                driver='{driver}',
-                url='jdbc:{scheme}://{server}:{port}{db}',
-                user='{user}', password='{password}',
-                dbtable=query)
-                """.format(scheme=parsed.scheme, server=parsed.hostname,
-                           db=parsed.path,
-                           port=parsed.port,
-                           driver=driver, user=qs_parsed.get('user', [''])[0],
-                           password=qs_parsed.get('password', [''])[0],
-                           table=self.metadata.get('command'),
-                           out=self.output))
+
+        code_jdbc = self.SUPPORTED_DRIVERS[parsed.scheme].format(
+            scheme=parsed.scheme, host=parsed.hostname,
+            db=parsed.path[1:],
+            port=parsed.port or 3306,
+            user=qs_parsed.get('user', [''])[0],
+            password=qs_parsed.get('password', [''])[0],
+            table=self.metadata.get('command'),
+            out=self.output)
         code.append(code_jdbc)
 
     # noinspection PyMethodMayBeStatic
@@ -341,6 +305,19 @@ class SaveOperation(Operation):
     FORMAT_PICKLE = 'PICKLE'
     FORMAT_CSV = 'CSV'
     FORMAT_JSON = 'JSON'
+    FORMAT_PARQUET = 'PARQUET'
+
+    USER_PARAM = 'user'
+    WORKFLOW_ID_PARAM = 'workflow_id'
+
+    PANDAS_TO_LIMONERO_DATA_TYPES = {
+        'object': "CHARACTER",
+        'datetime64[ns]': "DATETIME",
+        'float64': "DOUBLE",
+        'float32': "FLOAT",
+        'int64': "LONG",
+        'int32': "INTEGER",
+    }
 
     def __init__(self, parameters, named_inputs, named_outputs):
         Operation.__init__(self, parameters, named_inputs, named_outputs)
@@ -352,17 +329,13 @@ class SaveOperation(Operation):
                         att, self.__class__))
 
         self.name = parameters.get(self.NAME_PARAM)
+        self.tags = parameters.get(self.TAGS_PARAM)
         self.format = parameters.get(self.FORMAT_PARAM, self.FORMAT_CSV)
         self.path = parameters.get(self.PATH_PARAM, '.')
         self.mode = parameters.get(self.OVERWRITE_MODE_PARAM, self.MODE_ERROR)
-
-        if self.mode == "overwrite":
-            self.mode = "w"
-        elif self.mode == "append":
-            self.mode = "a"
-        else:
-            # NEED CHECK
-            self.mode = "w"
+        self.storage_id = parameters.get(self.STORAGE_ID_PARAM)
+        self.user = parameters.get(self.USER_PARAM)
+        self.workflow_id = parameters.get(self.WORKFLOW_ID_PARAM)
 
         self.header = parameters.get(self.HEADER_PARAM, False) in (1, '1', True)
 
@@ -371,38 +344,162 @@ class SaveOperation(Operation):
                                                  self.order))
 
         self.filename = self.name
-        # if len(self.path) > 0:
-        #     self.filename = '/'+self.name
-        # else:
-        #     self.filename = '/'+self.path+'/'+self.name
-
         self.has_code = len(self.named_inputs) == 1
-
-    def get_data_out_names(self, sep=','):
-        return ''
-
-    def get_output_names(self, sep=", "):
-        return self.output
 
     def generate_code(self):
 
-        code_save = ""
-        if self.FORMAT_CSV == self.format:
-            code_save = """
-                {input}.to_csv('{output}', sep=',', mode='{mode}',
-                    header={header}, index=False, encoding='utf-8')
-                
-                {tmp} = None
-                """.format(tmp=self.output,
-                           output=self.filename,
-                           input=self.named_inputs['input data'],
-                           mode=self.mode,
-                           header=self.header)
-        elif self.FORMAT_JSON == self.format:
-            code_save = """
-            {input}.to_json('{output}', orient='records', encoding='utf-8')
-            {tmp} = None
-            """.format(tmp=self.output, output=self.filename,
-                       input=self.named_inputs['input data'])
+        limonero_config = \
+            self.parameters['configuration']['juicer']['services']['limonero']
+        url = '{}'.format(limonero_config['url'], self.mode)
+        token = str(limonero_config['auth_token'])
+        storage = limonero_service.get_storage_info(url, token, self.storage_id)
 
-        return dedent(code_save)
+        if storage['type'] != 'HDFS':
+            raise ValueError(_('Storage type not supported: {}').format(
+                storage['type']))
+
+        if storage['url'].endswith('/'):
+            storage['url'] = storage['url'][:-1]
+        if self.path.endswith('/'):
+            self.path = self.path[:-1]
+
+        if self.path.startswith('/'):
+            self.path = self.path[1:]
+
+        final_url = '{}/limonero/user_data/{}/{}/{}'.format(
+            storage['url'],
+            self.user['id'],
+            self.path,
+            self.name.replace(' ', '_'))
+
+        if self.format == self.FORMAT_CSV and not final_url.endswith('.csv'):
+            final_url += '.csv'
+        elif self.format == self.FORMAT_JSON and not final_url.endswith(
+                '.json'):
+            final_url += '.json'
+        elif self.format == self.FORMAT_PARQUET and not final_url.endswith(
+                '.parquet'):
+            final_url += '.parquet'
+
+        df_input = self.named_inputs['input data']
+        code_template = """
+            path = '{{path}}'
+            {%- if scheme == 'hdfs' %}
+            fs = pa.hdfs.connect('{{hdfs_server}}', {{hdfs_port}})
+            exists = fs.exists(path)
+            {%- elif scheme == 'file' %}
+            exists = os.path.exists(path)
+            {%- endif %}
+
+            mode = '{{mode}}'
+            if mode not in ('error', 'ignore', 'overwrite'):
+                raise ValueError('{{error_invalid_mode}}')
+            if exists:
+                if mode == 'error':
+                    raise ValueError('{{error_file_exists}}')
+                elif mode == 'ignore':
+                    emit_event(name='update task',
+                        message='{{warn_ignored}}',
+                        status='COMPLETED',
+                        identifier='{{task_id}}')
+
+            if not exists or mode == 'overwrite':
+                {%- if scheme == 'hdfs'%}
+                fs.delete(path, False)
+                f = fs.open(path, 'wb')
+                {%- elif scheme == 'file' %}
+                if exists:
+                    os.remove(path)
+                parent_dir = os.path.dirname(path)
+                if not os.path.exists(parent_dir):
+                    os.makedirs(parent_dir)
+                f = open(path, 'wb')
+                {%- endif %}
+                {%- if format == FORMAT_CSV %}
+                {{input}}.to_csv(f, sep=',', mode='w',
+                    header={{header}}, index=False, encoding='utf-8')
+                {%- elif format == FORMAT_PARQUET %}
+                {{input}}.to_parquet(f, engine='pyarrow')
+                {%- elif format == FORMAT_JSON %}
+                {{input}}.to_json(f, orient='records')
+                {%- endif %}
+                f.close()
+
+
+            # Code to update Limonero metadata information
+            from juicer.service.limonero_service import register_datasource
+            types_names = {{data_types}}
+
+            attributes = []
+            for attr in {{input}}.columns:
+                type_name = {{input}}.dtypes[attr]
+                precision = None
+                scale = None
+                attributes.append({
+                  'enumeration': 0,
+                  'feature': 0,
+                  'label': 0,
+                  'name': attr,
+                  'type': types_names[str(type_name)],
+                  'nullable': True,
+                  'metadata': None,
+                  'precision': precision,
+                  'scale': scale
+                })
+            parameters = {
+                'name': "{{name}}",
+                'enabled': 1,
+                'is_public': 0,
+                'format': "{{format}}",
+                'storage_id': {{storage}},
+                'description': "{{description}}",
+                'user_id': "{{user_id}}",
+                'user_login': "{{user_login}}",
+                'user_name': "{{user_name}}",
+                'workflow_id': "{{workflow_id}}",
+                'task_id': '{{task_id}}',
+                'url': "{{final_url}}",
+                'attributes': attributes,
+                'tags': ','.join({{tags}})
+            }
+            register_datasource('{{url}}', parameters, '{{token}}', 'overwrite')
+        """
+        parsed = urlparse(final_url)
+        template = Environment(loader=BaseLoader).from_string(
+            code_template)
+        path = parsed.path
+
+        ctx = dict(path=path,
+                   hdfs_server=parsed.hostname,
+                   hdfs_port=parsed.port,
+                   scheme=parsed.scheme,
+                   name=self.name,
+                   mode=self.mode,
+                   storage=self.storage_id,
+                   description=_('Data source generated by workflow {}').format(
+                       self.workflow_id),
+                   workflow_id=self.workflow_id,
+                   format=self.format,
+                   header=self.header,
+                   user_name=self.user['name'],
+                   user_id=self.user['id'],
+                   user_login=self.user['login'],
+                   tags=repr(self.tags),
+                   FORMAT_CSV=self.FORMAT_CSV,
+                   FORMAT_PICKLE=self.FORMAT_PICKLE,
+                   FORMAT_JSON=self.FORMAT_JSON,
+                   FORMAT_PARQUET=self.FORMAT_PARQUET,
+                   data_types=json.dumps(self.PANDAS_TO_LIMONERO_DATA_TYPES),
+                   final_url=final_url,
+                   input=df_input,
+                   token=token,
+                   url=url,
+                   error_file_exists=_('File already exists'),
+                   warn_ignored=_('File not written (already exists)'),
+                   error_invalid_mode=_('Invalid mode {}').format(self.mode),
+                   uuid=uuid.uuid4().get_hex(),
+                   storage_url=storage['url'],
+                   task_id=self.parameters['task_id'],
+                   )
+
+        return dedent(template.render(ctx))
