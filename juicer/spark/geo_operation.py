389d44ea21964870d3c33f19ac82e12b76a24fea
Walter dos Santos Filho <waltersf@gmail.com>
2018-09-19 17:52:20 -0300

    Support for parquet file, improve in geo operations

--- juicer/spark/geo_operation.py
+++ juicer/spark/geo_operation.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import json
 from itertools import izip_longest
 from textwrap import dedent
 
+from jinja2 import Environment, BaseLoader
 from juicer.operation import Operation
 from juicer.spark.data_operation import DataReaderOperation
 
@@ -41,12 +41,32 @@ class ReadShapefile(DataReaderOperation):
         LAT,LON:
             points.append([point[1], point[0]])
         """
-        code = """
+
+        code_template = dedent("""
             import shapefile
+            import zipfile
             from io import BytesIO
-            # reload(sys)
-            # sys.setdefaultencoding('utf-8')
-            shp_file = '{url}'
+            shp_file = '{{url}}'
+            shp_io = None
+            dbf_io = None
+            shx_io = None
+            {%- if zipped %}
+            # shp and dbf files must be present in the zip file
+            # shx is optional
+            memory_data = BytesIO(spark_session.sparkContext.binaryFiles(
+                shp_file).collect()[0][1])
+
+            z = zipfile.ZipFile(memory_data)
+            for file_name in z.namelist():
+                if file_name.endswith('.shp'):
+                    shp_io = BytesIO(z.open(file_name).read())
+                elif file_name.endswith('.shx'):
+                    shx_io = BytesIO(z.open(file_name).read())
+                elif file_name.endswith('.dbf'):
+                    dbf_io = BytesIO(z.open(file_name).read())
+            if not all([shp_io, dbf_io]):
+                raise ValueError('{{invalid_shp}}')
+            {%- else %}
             dbf_file = re.sub('.shp$', '.dbf', shp_file)
             shp_content = spark_session.sparkContext.binaryFiles(
                 shp_file).collect()
@@ -54,11 +74,15 @@ class ReadShapefile(DataReaderOperation):
                 dbf_file).collect()
             shp_io = BytesIO(shp_content[0][1])
             dbf_io = BytesIO(dbf_content[0][1])
+            {%- endif %}
 
-            shp_object = shapefile.Reader(shp=shp_io, dbf=dbf_io)
-            records = shp_object.records()
+            shp_object = shapefile.Reader(shp=shp_io, dbf=dbf_io, shx=shx_io)
             records = shp_object.shapeRecords()
-            header = {attrs}
+            header = [
+                {%- for attr in attrs %}
+                '{{attr.name}}',
+                {%- endfor %}
+            ]
             header = types.StructType(
                 [types.StructField(h, types.StringType(), False)
                              for h in header])
@@ -68,13 +92,16 @@ class ReadShapefile(DataReaderOperation):
             for shape_record in records:
                 data.append(shape_record.record +
                     [shape_record.shape.points])
-            {out} = spark_session.createDataFrame(data, header)
-        """.format(url=self.metadata['url'],
-                   attrs=json.dumps([a['name'] for a in
-                                     self.metadata.get('attributes', [])]),
-                   out=self.output)
-
-        return dedent(code)
+            {{out}} = spark_session.createDataFrame(data, header)
+        """)
+        ctx = dict(
+            zipped=self.metadata['url'].endswith('.zip'),
+            invalid_shp=_('Invalid zipped shapefile. It must contains '
+                          'both *.shp and *.dbf files.'),
+            url=self.metadata['url'], out=self.output,
+            attrs=self.metadata.get('attributes', []))
+        template = Environment(loader=BaseLoader).from_string(code_template)
+        return template.render(ctx)
 
 
 class GeoWithin(Operation):
@@ -86,7 +113,7 @@ class GeoWithin(Operation):
 
     def __init__(self, parameters, named_inputs, named_outputs):
         Operation.__init__(self, parameters, named_inputs, named_outputs)
-        self.polygon_column = parameters[self.POLYGON_POINTS_COLUMN_PARAM]
+        self.points_column = parameters[self.POLYGON_POINTS_COLUMN_PARAM]
         self.attributes = parameters[self.POLYGON_ATTRIBUTES_COLUMN_PARAM]
 
         self.alias = [
@@ -102,8 +129,10 @@ class GeoWithin(Operation):
         self.lat_column = parameters[self.TARGET_LAT_COLUMN_PARAM]
         self.lon_column = parameters[self.TARGET_LON_COLUMN_PARAM]
 
+        self.output = self.named_outputs.get('output data',
+                                             'out_{}'.format(self.order))
         if len(self.lat_column) == 0 or len(self.lon_column) == 0 or len(
-                self.polygon_column) == 0:
+                self.points_column) == 0:
             raise ValueError(
                 _('Values for latitude and longitude columns must be informed'))
 
@@ -112,8 +141,8 @@ class GeoWithin(Operation):
             from matplotlib.path import Path
             import pyqtree
 
-            schema = [s.name for s in {0}.schema]
-            shp_object = {0}.collect()
+            schema = [s.name for s in {geo}.schema]
+            shp_object = {geo}.collect()
             bcast_shapefile = spark_session.sparkContext.broadcast(
                 shp_object)
 
@@ -146,6 +175,7 @@ class GeoWithin(Operation):
             broad_casted_sp_index = spark_session.sparkContext.broadcast(
                 sp_index)
 
+            counter = 0
             def get_first_polygon(lat, lng):
                 x = float(lat)
                 y = float(lng)
@@ -155,22 +185,31 @@ class GeoWithin(Operation):
 
                 for shp_inx in matches:
                     row = bcast_shapefile.value[shp_inx]
-                    polygon = Path(row['points'])
+                    p_polygon = Path(row['{points_column}'])
                     # Here it uses longitude, latitude
-                    if polygon.contains_point([y, x]):
-                        return [col for col in row]
+                    if p_polygon.contains_point([y, x]):
+                        return [c for c in row]
                 return [None] * len(bcast_shapefile.value[0])
 
-            shapefile_features_count= len(bcast_shapefile.value[0])
             udf_get_first_polygon = functions.udf(
                 get_first_polygon, types.ArrayType(types.StringType()))
-            within = {2}.withColumn(
-                "polygon_position", udf_get_first_polygon(functions.col('{3}'),
-                                                        functions.col('{4}')))
-            {5} = within.select(within.columns +
-                [within.polygon_position[i].alias(schema[i])
-                    for i in xrange(shapefile_features_count)])
-        """.format(self.named_inputs['geo data'], self.polygon_column[0],
-                   self.named_inputs['input data'], self.lat_column[0],
-                   self.lon_column[0], self.named_outputs['output data'])
+            within = {input}.withColumn(
+                'tmp_polygon_data', udf_get_first_polygon(
+                    functions.col('{lat}'), functions.col('{lng}')))
+            aliases = {aliases}
+            attributes_to_add = {attributes}
+            {out} = within.select(within.columns +
+               [within.tmp_polygon_data[i].alias(aliases.pop())
+                for i, col in enumerate(schema)
+                if col in attributes_to_add])
+
+            {out} = {out}.drop('tmp_polygon_data')
+        """.format(geo=self.named_inputs['geo data'],
+                   points_column=self.points_column[0],
+                   input=self.named_inputs['input data'],
+                   lat=self.lat_column[0],
+                   lng=self.lon_column[0], out=self.output,
+                   aliases=repr(self.alias),
+                   attributes=self.attributes
+                   )
         return dedent(code)
