608a43c3ab05465081c827a169a5269d74c1c75a
Eric <ericstefano12@gmail.com>
2020-05-26 18:37:02 -0300

    fixed names, made code more clear

--- tests/scikit_learn/etl/test_split.py
+++ tests/scikit_learn/etl/test_split.py
@@ -25,35 +25,13 @@ def test_split_data_integrity():
     result = util.execute(instance.generate_code(),
                           {'df': df[1]})
 
-    x = [result['split_1_task_1'], result['split_2_task_1']]
-    y = pd.concat(x, axis=0, join='outer', ignore_index=False, keys=None,
-                  levels=None, names=None, verify_integrity=False, copy=True)
-    z = y.sort_index()
+    result = [result['split_1_task_1'], result['split_2_task_1']]
+    result = pd.concat(result, axis=0, join='outer', ignore_index=False, keys=None,
+                       levels=None, names=None, verify_integrity=False, copy=True)
+    result = result.sort_index()
 
-    assert len(y) == slice_size
-    assert z.equals(util.iris(['sepallength', 'sepalwidth',
-                               'petallength', 'petalwidth'], size=slice_size))
-
-
-def test_split_success():
-    slice_size = 10
-    df = ['df', util.iris(['sepallength', 'sepalwidth',
-                           'petallength', 'petalwidth'], slice_size)]
-
-    arguments = {
-        'parameters': {},
-        'named_inputs': {
-            'input data': df[0],
-        },
-        'named_outputs': {
-            'split 1': 'split_1_task_1',
-            'split 2': 'split_2_task_1'
-        }
-    }
-    instance = SplitOperation(**arguments)
-    result = util.execute(instance.generate_code(),
-                          {'df': df[1]})
-    assert len(result['split_1_task_1']) and len(result['split_2_task_1']) != slice_size
+    assert len(result) == slice_size
+    assert result.equals(df[1])
 
 
 def test_split_randomness():
@@ -75,15 +53,37 @@ def test_split_randomness():
     result = util.execute(instance.generate_code(),
                           {'df': df[1]})
 
-    x = [result['split_1_task_1'], result['split_2_task_1']]
-    y = pd.concat(x, axis=0, join='outer', ignore_index=False, keys=None,
-                  levels=None, names=None, verify_integrity=False, copy=True)
+    result = [result['split_1_task_1'], result['split_2_task_1']]
+    result = pd.concat(result, axis=0, join='outer', ignore_index=False, keys=None,
+                       levels=None, names=None, verify_integrity=False, copy=True)
 
-    assert not y.equals(util.iris(['sepallength', 'sepalwidth',
-                                   'petallength', 'petalwidth'], size=slice_size))
+    assert not result.equals(df[1])
 
 
-def test_split_sucess_one_slice():
+def test_split_success():
+    slice_size = 10
+    df = ['df', util.iris(['sepallength', 'sepalwidth',
+                           'petallength', 'petalwidth'], slice_size)]
+
+    arguments = {
+        'parameters': {},
+        'named_inputs': {
+            'input data': df[0],
+        },
+        'named_outputs': {
+            'split 1': 'split_1_task_1',
+            'split 2': 'split_2_task_1'
+        }
+    }
+    instance = SplitOperation(**arguments)
+    result = util.execute(instance.generate_code(),
+                          {'df': df[1]})
+
+    assert len(result['split_1_task_1']) == 5
+    assert len(result['split_2_task_1']) == 5
+
+
+def test_split_success_one_slice():
     slice_size = 1
     df = ['df', util.iris(['sepallength', 'sepalwidth',
                            'petallength', 'petalwidth'], slice_size)]
@@ -117,19 +117,24 @@ def test_split_fail_by_seed():
             'input data': df[0],
         },
         'named_outputs': {
+            'split 1': 'split_1_task_1',
+            'split 2': 'split_2_task_1'
         }
     }
     instance = SplitOperation(**arguments)
-    with pytest.raises(ValueError) as InvalidSeed:
+    with pytest.raises(ValueError) as invalid_seed:
         result = util.execute(instance.generate_code(),
                               {'df': df[1]})
-    print(InvalidSeed)
+    print(invalid_seed)
 
 
 def test_weird_split_by_weights_and_slice():
     """
     This happens when the 'slice_size' is between 1 and 10
     and the 'weights' value is between -9 and 9
+
+    The weights don't make a difference in these cases
+
     I don't know if it's a bug or not
     """
     slice_size = 10
@@ -142,22 +147,18 @@ def test_weird_split_by_weights_and_slice():
             'input data': df[0],
         },
         'named_outputs': {
+            'split 1': 'split_1_task_1',
+            'split 2': 'split_2_task_1'
         }
     }
     instance = SplitOperation(**arguments)
     result = util.execute(instance.generate_code(),
                           {'df': df[1]})
-    with pytest.raises(AssertionError) as Weird:
-        assert len(result['split_1_task_1']) and len(result['split_2_task_1']) != slice_size
-    print(Weird)
+
+    assert len(result['split_2_task_1']) == slice_size
 
 
 def test_split_success_no_output_implies_no_code():
-    """
-    Change SplitOperation: it shouldn't generate
-    code if there is not output!
-    Does this happen to every operation???
-    """
     slice_size = 10
     df = ['df', util.iris(['sepallength', 'sepalwidth',
                            'petallength', 'petalwidth'], slice_size)]
@@ -167,12 +168,10 @@ def test_split_success_no_output_implies_no_code():
         'named_inputs': {
             'input data': df[0],
         },
-        'named_outputs': {
-        }
+        'named_outputs': {}
     }
     instance = SplitOperation(**arguments)
     result = util.execute(instance.generate_code(),
                           {'df': df[1]})
-    with pytest.raises(AssertionError) as HasCode:
-        assert not instance.has_code
-    print(HasCode)
+
+    assert not instance.has_code
