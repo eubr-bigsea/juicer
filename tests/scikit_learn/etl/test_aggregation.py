18288411c8cc5731c465318bd35445c367117ebe
Eric <ericstefano12@gmail.com>
2020-06-01 12:31:43 -0300

    aggregation operation is broken(?)

--- tests/scikit_learn/etl/test_aggregation.py
+++ tests/scikit_learn/etl/test_aggregation.py
@@ -1,16 +1,22 @@
 from tests.scikit_learn import util
 from juicer.scikit_learn.etl_operation import AggregationOperation
+import pandas as pd
 import pytest
 
+
 # Aggregation
-# 
+#
+
 def test_aggregation_success():
     slice_size = 10
-    df = ['df', util.iris(['sepallength', 'sepalwidth', 
-        'petalwidth', 'petallength'], slice_size)]
+    df = ['df', util.iris('sepallength', slice_size)]
 
     arguments = {
-        'parameters': {},
+
+        'parameters': {
+            AggregationOperation.ATTRIBUTES_PARAM: 'sepallenght',
+            AggregationOperation.FUNCTION_PARAM:
+                [{'attribute': 'sepallenght', 'alias': 'media_sepallenght', 'f': 'avg'}]},
         'named_inputs': {
             'input data': df[0],
         },
@@ -18,7 +24,8 @@ def test_aggregation_success():
             'output data': 'out'
         }
     }
+
     instance = AggregationOperation(**arguments)
-    result = util.execute(instance.generate_code(), 
-                          dict([df]))
+    result = util.execute(instance.generate_code(),
+                          {'df': df[1]})
     assert result['out'].equals(util.iris(size=slice_size))
