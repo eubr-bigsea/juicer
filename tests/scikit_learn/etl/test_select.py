dc8168612dd42643addb24418b3356c1f49e28cd
gabriel <gabriel.valadao.meira@gmail.com>
2020-07-03 18:41:29 -0300

    esqueleto select test

--- tests/scikit_learn/etl/test_select.py
+++ tests/scikit_learn/etl/test_select.py
@@ -10,7 +10,7 @@ def test_select_success():
         'petalwidth', 'petallength'], slice_size)]
 
     arguments = {
-        'parameters': {},
+        'parameters': {'sepallength'},
         'named_inputs': {
             'input data': df[0],
         },
@@ -23,26 +23,35 @@ def test_select_success():
                           dict([df]))
     assert result['out'].equals(util.iris(size=slice_size))
 
-def test_select_fail_parameters_not_needed():
-    #como nao exige parametros deve falhar se fornecer parametro?
+def test_select_fail_missing_parameters():
+    #deve falhar ao nao receber parametros, não gerar códigou ou gerar uma saída vazia?
 
-def test_select_success_with_2_valid_named_inputs():
-    #em vez do input data ser so df[0] seria df[0] e df[1] por exemplo, devendo selecionar as duas colunas
+def test_select_success_valid_parameters():
+    # deve selecionar uma coluna da tabela
 
-def test_select_fail_invalid_and_valid_named_inputs():
-    #deve dar erro se for inserido um named input valido e um invalido ou vice e versa certo?
+def test_select_success_with_2_valid_parameters():
+    #deve selecionar duas colunas da tabela
+
+def test_select_fail_invalid_and_valid_parameters():
+    #deve dar erro se for inserido um parametro valido e um invalido ou vice e versa certo?
+
+def test_select_fail_invalid_parameter():
+    #deve dar erro ao ser inserido um parametro invalido
+
+def test_select_fail_2_invalid_parameters():
+    #deve dar erro ao serem inseridos 2 parametros invalidos
+
+def test_select_success_2_equal_parameters():
+    #se forem fornecidos 2 parametros iguais deve selecionar a coluna e não tem problema
 
 def test_select_fail_invalid_named_inputs():
-    #deve dar erro ao ser inserido um named input invalido
+    # deve dar erro ao ser inserido um named input invalido
 
-def test_select_fail_2_invalid_named_inputs():
-    #deve dar erro ao serem inseridos 2 named input invalidos
-
-def test_select_fail_2_equal_named_inputs():
-    #deve dar erro ao inserir 2 named_inputs iguais ou selecionar a mesma coluna 2 vezes?
+def test_select_fail_2_named_inputs():
+    # deve dar erro pois select só aceita um named input
 
 def test_select_success_missing_input_inplies_no_code():
-    #deve dar sucessso porem sem gerar codigo
+    #deve dar sucessso porem sem gerar codigo, has_code() deve retornar False
 
 def test_select_success_missing_output_inplies_no_code():
     #deve dar sucessso porem sem gerar codigo
