43bc1f2411ee2f4cd3164b6d58e0c6719981ecd4
gabriel <gabriel.valadao.meira@gmail.com>
2020-07-17 18:48:07 -0300

    esqueleto select test

--- tests/scikit_learn/etl/test_select.py
+++ tests/scikit_learn/etl/test_select.py
@@ -3,7 +3,6 @@ from juicer.scikit_learn.etl_operation import SelectOperation
 import pytest
 
 # Select
-#,
 
 
 def test_select_success():
@@ -30,7 +29,7 @@ def test_select_success():
     """
     result = util.execute(instance.generate_code(),
                           {'df': df[1]})
-    print(result)
+    #print(result)
 
     assert result['out'].equals(util.iris(['sepallength'],size=slice_size))
 
@@ -77,10 +76,9 @@ def test_select_success_with_2_valid_parameters():
     """
     result = util.execute(instance.generate_code(),
                           {'df': df[1]})
-    print(result)
+    #print(result)
 
     assert result['out'].equals(util.iris(['sepallength','sepalwidth'], size=slice_size))
-
 
 
 def test_select_fail_invalid_and_valid_parameters():
@@ -108,7 +106,7 @@ def test_select_fail_invalid_and_valid_parameters():
     print(instance.generate_code())
     print("\n=====================================\n")
     """
-    print(exc_info.value)
+    #print(exc_info.value)
     assert "['class'] not in index" in str(exc_info.value)
 
 def test_select_fail_invalid_parameter():
@@ -136,7 +134,7 @@ def test_select_fail_invalid_parameter():
     print(instance.generate_code())
     print("\n=====================================\n")
     """
-    print(exc_info.value)
+    #print(exc_info.value)
     assert "None of [Index(['class'], dtype='object')] are in the [columns]" in str(exc_info.value)
 
 def test_select_fail_2_invalid_parameters():
@@ -164,7 +162,7 @@ def test_select_fail_2_invalid_parameters():
     print(instance.generate_code())
     print("\n=====================================\n")
     """
-    print(exc_info.value)
+    #print(exc_info.value)
     assert "None of [Index(['class', 'class2'], dtype='object')] are in the [columns]" in str(exc_info.value)
 
 def test_select_success_2_equal_parameters():
@@ -184,26 +182,78 @@ def test_select_success_2_equal_parameters():
         }
     }
     instance = SelectOperation(**arguments)
-
+    """
     print("\n==========generated code============\n")
     print(instance.generate_code())
     print("\n=====================================\n")
-
+    """
     result = util.execute(instance.generate_code(),
                           {'df': df[1]})
-    print(result)
+    #print(result)
 
     assert result['out'].equals(util.iris(['sepallength','sepallength'], size=slice_size))
 
-#se forem fornecidos 2 parametros iguais deve selecionar a coluna e não tem problema
 
 def test_select_fail_invalid_named_inputs():
-    pass
-# deve dar erro ao ser inserido um named input invalido
+    slice_size = 10
+    df = ['df', util.iris(['sepallength', 'sepalwidth',
+                           'petalwidth', 'petallength'], slice_size)]
+    er = ['error',[]]
+    arguments = {
+        'parameters': {
+            SelectOperation.ATTRIBUTES_PARAM: ['sepallength']
+        },
+        'named_inputs': {
+            'input data': er[0],
+        },
+        'named_outputs': {
+            'output projected data': 'out'
+        }
+    }
+    instance = SelectOperation(**arguments)
+    """
+    print("\n==========generated code============\n")
+    print(instance.generate_code())
+    print("\n=====================================\n")
+    """
+    with pytest.raises(TypeError) as exc_info:
+        result = util.execute(instance.generate_code(),
+                          {'error': er[1]})
+
+    #print(exc_info.value)
+    assert 'list indices must be integers or slices, not list' in str(exc_info.value)
+
 
 def test_select_fail_2_named_inputs():
-    pass
-# deve dar erro pois select só aceita um named input
+    slice_size = 10
+    df = ['df', util.iris(['sepallength', 'sepalwidth',
+                           'petalwidth', 'petallength'], slice_size)]
+    er = ['error', []]
+    er1 = ['error1', []]
+    arguments = {
+        'parameters': {
+            SelectOperation.ATTRIBUTES_PARAM: ['sepallength']
+        },
+        'named_inputs': {
+            'input data': [er[0],er1[0]],
+        },
+        'named_outputs': {
+            'output projected data': 'out'
+        }
+    }
+    instance = SelectOperation(**arguments)
+    """
+    print("\n==========generated code============\n")
+    print(instance.generate_code())
+    print("\n=====================================\n")
+    """
+    with pytest.raises(TypeError) as exc_info:
+        result = util.execute(instance.generate_code(),
+                              {'error': er[1]} and {'error1': er1[1]})
+
+    #print(exc_info.value)
+    assert 'list indices must be integers or slices, not list' in str(exc_info.value)
+
 
 def test_select_success_missing_input_inplies_no_code():
     #deve dar sucessso porem sem gerar codigo, has_code() deve retornar False
@@ -221,6 +271,8 @@ def test_select_success_missing_input_inplies_no_code():
     }
     instance = SelectOperation(**arguments)
     assert not instance.has_code
+
+
 def test_select_success_missing_output_inplies_no_code():
     #deve dar sucessso porem sem gerar codigo
     slice_size = 10
