556205990d3b721588680078f9f37304b5a3863d
Walter dos Santos Filho <waltersf@gmail.com>
2020-08-20 15:44:21 -0300

    Change how table is generated

--- juicer/spark/vis_operation.py
+++ juicer/spark/vis_operation.py
@@ -1026,11 +1026,9 @@ class TableVisualizationModel(VisualizationModel):
         Returns data as tabular (list of lists in Python).
         """
         if self.column_names:
-            rows = self.data.limit(500).select(*self.column_names).rdd.map(
-                dataframe_util.convert_to_python).collect()
+            rows = self.data.limit(500).select(*self.column_names).collect()
         else:
-            rows = self.data.limit(500).rdd.map(
-                dataframe_util.convert_to_python).collect()
+            rows = self.data.limit(500).collect()
 
         return {"rows": rows,
                 "attributes": self.get_column_names().split(',')}
