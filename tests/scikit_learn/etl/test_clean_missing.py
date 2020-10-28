1c5825955fc38c5fdbe0ac8beec40fbe90fafe48
Eric <ericstefano12@gmail.com>
2020-10-28 16:16:23 -0300

    minor changes to tests

--- tests/scikit_learn/etl/test_clean_missing.py
+++ tests/scikit_learn/etl/test_clean_missing.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import pytest
 
+
 # pd.set_option('display.max_rows', None)
 # pd.set_option('display.max_columns', None)
 # pd.set_option('display.max_colwidth', None)
--- tests/scikit_learn/etl/test_distinct.py
+++ tests/scikit_learn/etl/test_distinct.py
@@ -8,6 +8,7 @@ import pytest
 # pd.set_option('display.max_columns', None)
 # pd.set_option('display.max_colwidth', None)
 
+
 # Distinct
 #
 #
