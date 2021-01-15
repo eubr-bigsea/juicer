6e99e7dcb088e27fcd6dd18774af6b30f1c363c3
Lucas Ponce <lucasmsp@gmail.com>
2021-01-15 16:07:14 -0300

    adding new association rules generator

--- juicer/spark/custom_library/__init__.py
+++ juicer/spark/custom_library/__init__.py
@@ -2,7 +2,8 @@
 
 from juicer.spark.custom_library.kmetamodes import \
     IncrementalPartitionedKMetaModes
-
+from juicer.spark.custom_library.association_rules import \
+                LemonadeAssociativeRules
 """
 Custom Pyspark Library.
 
