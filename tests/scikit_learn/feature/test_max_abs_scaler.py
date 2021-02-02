b5f3a8783dc85e6486db078b1b9a81c53ca578cc
eric <ericstefano12@gmail.com>
2021-02-02 16:27:52 -0300

    MaxAbsScalerOperation tests revision: -removed scaler method -changed assertions -added assertions

--- tests/scikit_learn/feature/test_max_abs_scaler.py
+++ tests/scikit_learn/feature/test_max_abs_scaler.py
@@ -1,18 +1,13 @@
 from tests.scikit_learn import util
 from juicer.scikit_learn.feature_operation import MaxAbsScalerOperation
 import pytest
+from sklearn.preprocessing import MaxAbsScaler
 import pandas as pd
 
 
 # pd.set_option('display.max_rows', None)
 # pd.set_option('display.max_columns', None)
 # pd.set_option('display.max_colwidth', None)
-
-
-def scaler(df, cols):
-    return [[df.loc[idx, col] / df[col].max() for col in cols]
-            for idx in df[cols].index]
-
 
 # MaxAbsScaler
 #
@@ -36,12 +31,21 @@ def test_max_abs_scaler_success():
     }
     instance = MaxAbsScalerOperation(**arguments)
     result = util.execute(util.get_complete_code(instance), {'df': df})
-    data = {'scaled_1': scaler(df, ['sepalwidth', 'petalwidth'])}
-    data = pd.DataFrame(data)
+    model_1 = MaxAbsScaler()
 
     assert test_df.max()['sepalwidth'] == 3.9
     assert test_df.max()['petalwidth'] == 0.4
-    assert result['out'].loc[:, 'scaled_1'].equals(data.loc[:, 'scaled_1'])
+    assert not result['out'].equals(test_df)
+    assert str(result['model_1']) == str(model_1)
+    assert """
+model_1 = MaxAbsScaler()
+X_train = get_X_train_data(df, ['sepalwidth', 'petalwidth'])
+model_1.fit(X_train)
+
+out = df
+out['scaled_1'] = model_1.transform(X_train).tolist()
+""" == \
+           instance.generate_code()
 
 
 def test_max_abs_scaler_alias_param_success():
