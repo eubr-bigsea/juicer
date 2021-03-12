bce3794a9b0b7598f499fec531902f7d5b4217e3
eric <ericstefano12@gmail.com>
2021-03-12 20:45:59 -0300

    Feature tests Revamp: -Modified execution results assertions -Added dedent() to code generation assertions -Completed fail assertions sentences -Removed redundant fail assertions/tests

--- tests/scikit_learn/feature/test_feature_assembler.py
+++ tests/scikit_learn/feature/test_feature_assembler.py
@@ -1,5 +1,6 @@
 from tests.scikit_learn import util
 from juicer.scikit_learn.feature_operation import FeatureAssemblerOperation
+from textwrap import dedent
 import pytest
 import pandas as pd
 import numpy as np
@@ -33,12 +34,20 @@ def test_feature_assembler_success():
     result = util.execute(instance.generate_code(),
                           {'df': df})
 
-    test_out = {'FeatureField': [[3.5, 0.2], [3.0, 0.2], [3.2, 0.2], [3.1, 0.2],
-                                 [3.6, 0.2], [3.9, 0.4], [3.4, 0.3], [3.4, 0.2],
-                                 [2.9, 0.2], [3.1, 0.1]]}
-    test_out = pd.DataFrame(test_out)
-    test_out = pd.concat([test_df, test_out], axis=1)
-    assert result['out'].equals(test_out)
+    cols = ['sepalwidth', 'petalwidth']
+    test_df = test_df.dropna(subset=cols)
+    test_df['FeatureField'] = test_df[cols].to_numpy().tolist()
+    assert result['out'].equals(test_df)
+    assert dedent("""
+    cols = ['sepalwidth', 'petalwidth']
+    if df[cols].dtypes.all() == np.object:
+        raise ValueError("Input 'df' must contain numeric values"
+        " only for task <class 'juicer.scikit_learn.feature_operation.FeatureAssemblerOperation'>")
+           
+    df_without_na = df.dropna(subset=cols)
+    out = df_without_na
+    out['FeatureField'] = df_without_na[cols].to_numpy().tolist()
+    """) == instance.generate_code()
 
 
 def test_feature_assembler_alias_param_success():
@@ -61,12 +70,20 @@ def test_feature_assembler_alias_param_success():
     result = util.execute(instance.generate_code(),
                           {'df': df})
 
-    test_out = {'Feat': [[3.5, 0.2], [3.0, 0.2], [3.2, 0.2], [3.1, 0.2],
-                         [3.6, 0.2], [3.9, 0.4], [3.4, 0.3], [3.4, 0.2],
-                         [2.9, 0.2], [3.1, 0.1]]}
-    test_out = pd.DataFrame(test_out)
-    test_out = pd.concat([test_df, test_out], axis=1)
-    assert result['out'].equals(test_out)
+    cols = ['sepalwidth', 'petalwidth']
+    test_df = test_df.dropna(subset=cols)
+    test_df['Feat'] = test_df[cols].to_numpy().tolist()
+    assert result['out'].equals(test_df)
+    assert dedent("""
+    cols = ['sepalwidth', 'petalwidth']
+    if df[cols].dtypes.all() == np.object:
+        raise ValueError("Input 'df' must contain numeric values"
+        " only for task <class 'juicer.scikit_learn.feature_operation.FeatureAssemblerOperation'>")
+
+    df_without_na = df.dropna(subset=cols)
+    out = df_without_na
+    out['Feat'] = df_without_na[cols].to_numpy().tolist()
+    """) == instance.generate_code()
 
 
 def test_feature_assembler_na_drop_success():
@@ -95,15 +112,20 @@ def test_feature_assembler_na_drop_success():
     instance = FeatureAssemblerOperation(**arguments)
     result = util.execute(instance.generate_code(),
                           {'df': df})
+    cols = ['sepalwidth', 'petalwidth']
+    test_df = test_df.dropna(subset=cols)
+    test_df['FeatureField'] = test_df[cols].to_numpy().tolist()
+    assert result['out'].equals(test_df)
+    assert dedent("""
+    cols = ['sepalwidth', 'petalwidth']
+    if df[cols].dtypes.all() == np.object:
+        raise ValueError("Input 'df' must contain numeric values"
+        " only for task <class 'juicer.scikit_learn.feature_operation.FeatureAssemblerOperation'>")
 
-    test_out = {
-        'FeatureField': [np.NaN, np.NaN, np.NaN, [3.1, 0.2], [3.6, 0.2],
-                         [3.9, 0.4], [3.4, 0.3], [3.4, 0.2], [2.9, 0.2],
-                         [3.1, 0.1]]}
-    test_out = pd.DataFrame(test_out)
-    test_out = pd.concat([test_df, test_out], axis=1)
-    test_out.drop([0, 1, 2], axis=0, inplace=True)
-    assert result['out'].equals(test_out)
+    df_without_na = df.dropna(subset=cols)
+    out = df_without_na
+    out['FeatureField'] = df_without_na[cols].to_numpy().tolist()
+    """) == instance.generate_code()
 
 
 def test_feature_assembler_no_output_implies_no_code_success():
@@ -147,67 +169,8 @@ def test_feature_assembler_missing_attributes_param_fail():
     }
     with pytest.raises(ValueError) as val_err:
         FeatureAssemblerOperation(**arguments)
-    assert "Parameters 'attributes' must be informed for task" in str(
-        val_err.value)
-
-
-def test_feature_assembler_missing_multiplicity_param_fail():
-    df = util.iris(['sepallength', 'sepalwidth',
-                    'petalwidth', 'petallength'], size=10)
-    arguments = {
-        'parameters': {'attributes': ['sepalwidth', 'petalwidth']},
-        'named_inputs': {
-            'input data': 'df',
-        },
-        'named_outputs': {
-            'output data': 'out'
-        }
-    }
-    instance = FeatureAssemblerOperation(**arguments)
-    with pytest.raises(KeyError) as key_err:
-        util.execute(instance.generate_code(),
-                     {'df': df})
-    assert "'multiplicity'" in str(key_err.value)
-
-
-def test_feature_assembler_invalid_attributes_param_fail():
-    df = util.iris(['sepallength', 'sepalwidth',
-                    'petalwidth', 'petallength'], size=10)
-    arguments = {
-        'parameters': {'attributes': 'invalid',
-                       'multiplicity': {'input data': 0}},
-        'named_inputs': {
-            'input data': 'df',
-        },
-        'named_outputs': {
-            'output data': 'out'
-        }
-    }
-    instance = FeatureAssemblerOperation(**arguments)
-    with pytest.raises(NameError) as nam_err:
-        util.execute(instance.generate_code(),
-                     {'df': df})
-    assert "name 'invalid' is not defined" in str(nam_err)
-
-
-def test_feature_assembler_invalid_multiplicity_param_fail():
-    df = util.iris(['sepallength', 'sepalwidth',
-                    'petalwidth', 'petallength'], size=10)
-    arguments = {
-        'parameters': {'attributes': ['sepalwidth', 'petalwidth'],
-                       'multiplicity': 'invalid'},
-        'named_inputs': {
-            'input data': 'df',
-        },
-        'named_outputs': {
-            'output data': 'out'
-        }
-    }
-    instance = FeatureAssemblerOperation(**arguments)
-    with pytest.raises(TypeError) as typ_err:
-        util.execute(instance.generate_code(),
-                     {'df': df})
-    assert "string indices must be integers" in str(typ_err)
+    assert f"Parameters 'attributes' must be informed for task" \
+           f" {FeatureAssemblerOperation}" in str(val_err.value)
 
 
 def test_feature_assembler_invalid_dtype_input_fail():
@@ -238,5 +201,5 @@ def test_feature_assembler_invalid_dtype_input_fail():
     with pytest.raises(ValueError) as val_err:
         util.execute(instance.generate_code(),
                      {'df': df})
-    assert "Input 'df' must contain numeric values only for task" in str(
-        val_err.value)
+    assert f"Input 'df' must contain numeric values only for task" \
+           f" {FeatureAssemblerOperation}" in str(val_err.value)
