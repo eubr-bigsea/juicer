c2e414858e169a644599fad98b3c0d6cd397045f
Lucas Ponce <lucasmsp@gmail.com>
2021-03-15 12:43:27 -0300

    updating tests

--- tests/scikit_learn/feature/test_feature_assembler.py
+++ tests/scikit_learn/feature/test_feature_assembler.py
@@ -44,9 +44,8 @@ def test_feature_assembler_success():
         raise ValueError("Input 'df' must contain numeric values"
         " only for task <class 'juicer.scikit_learn.feature_operation.FeatureAssemblerOperation'>")
            
-    df_without_na = df.dropna(subset=cols)
-    out = df_without_na
-    out['FeatureField'] = df_without_na[cols].to_numpy().tolist()
+    out = df.dropna(subset=cols)
+    out['FeatureField'] = out[cols].to_numpy().tolist()
     """) == instance.generate_code()
 
 
@@ -80,27 +79,31 @@ def test_feature_assembler_alias_param_success():
         raise ValueError("Input 'df' must contain numeric values"
         " only for task <class 'juicer.scikit_learn.feature_operation.FeatureAssemblerOperation'>")
 
-    df_without_na = df.dropna(subset=cols)
-    out = df_without_na
-    out['Feat'] = df_without_na[cols].to_numpy().tolist()
+    out = df.dropna(subset=cols)
+    out['Feat'] = out[cols].to_numpy().tolist()
     """) == instance.generate_code()
 
 
 def test_feature_assembler_na_drop_success():
     """
     The chained assignment warnings / exceptions are aiming to inform the user
-    of a possibly invalid assignment. There may be false positives; situations
+    of a possibly invalid assignment. There is a false positives; situations
     where a chained assignment is inadvertently reported.
     https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#returning-a-view-versus-a-copy
+    In this case, the error is due to util.execute()
     """
     df = util.iris(['sepallength', 'sepalwidth',
                     'petalwidth', 'petallength'], size=10)
-    # Line below is causing a false positive warning (?)
+
     df.loc[0:2, 'sepalwidth'] = np.NaN
+
     test_df = df.copy()
+    cols = ['sepalwidth', 'petalwidth']
+    test_df = test_df.dropna(subset=cols)
+    test_df['FeatureField'] = test_df[cols].to_numpy().tolist()
 
     arguments = {
-        'parameters': {'attributes': ['sepalwidth', 'petalwidth'],
+        'parameters': {'attributes': cols,
                        'multiplicity': {'input data': 0}},
         'named_inputs': {
             'input data': 'df',
@@ -110,11 +113,7 @@ def test_feature_assembler_na_drop_success():
         }
     }
     instance = FeatureAssemblerOperation(**arguments)
-    result = util.execute(instance.generate_code(),
-                          {'df': df})
-    cols = ['sepalwidth', 'petalwidth']
-    test_df = test_df.dropna(subset=cols)
-    test_df['FeatureField'] = test_df[cols].to_numpy().tolist()
+    result = util.execute(instance.generate_code(), {'df': df})
     assert result['out'].equals(test_df)
     assert dedent("""
     cols = ['sepalwidth', 'petalwidth']
@@ -122,9 +121,8 @@ def test_feature_assembler_na_drop_success():
         raise ValueError("Input 'df' must contain numeric values"
         " only for task <class 'juicer.scikit_learn.feature_operation.FeatureAssemblerOperation'>")
 
-    df_without_na = df.dropna(subset=cols)
-    out = df_without_na
-    out['FeatureField'] = df_without_na[cols].to_numpy().tolist()
+    out = df.dropna(subset=cols)
+    out['FeatureField'] = out[cols].to_numpy().tolist()
     """) == instance.generate_code()
 
 
