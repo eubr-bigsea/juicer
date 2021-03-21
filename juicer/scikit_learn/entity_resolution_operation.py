f66a853fce43c57566f7b59dd769eeb4d3aa2431
Laura <laura.nunes.andrade@gmail.com>
2021-03-21 17:26:27 -0300

    Initial code for indexing operation.

--- juicer/scikit_learn/entity_resolution_operation.py
+++ juicer/scikit_learn/entity_resolution_operation.py
@@ -3,9 +3,7 @@
 from textwrap import dedent
 from juicer.operation import Operation
 import re
-from juicer.scikit_learn.util import get_X_train_data, get_label_data
 from juicer.util.template_util import *
-from juicer.scikit_learn.model_operation import AlgorithmOperation
 import json
 import time
 from random import random
@@ -18,14 +16,30 @@ class IndexingOperation(Operation):
     These pairs are called candidate links or candidate matches.
     There are several indexing algorithms available such as blocking and sorted neighborhood indexing.
     Parameters:
-    - att: list of the attributes that will be used to do de indexing.
+    - features: list of the attributes that will be used to do de indexing.
     - alg: the algorithm that will ne used for the indexing.
     """
 
-    ATTRIBUTES_PARAM = 'att'
+    ATTRIBUTES_PARAM = 'features'
     ALGORITHM_PARAM = 'alg'
 
     def __init__(self, parameters, named_inputs, named_outputs):
         Operation.__init__(self, parameters, named_inputs, named_outputs)
 
         self.name = 'entity_resolution.IndexingOperation'
+
+        self.has_code = any([len(self.named_outputs) > 0, self.contains_results()])
+        if self.has_code:
+            self.features = parameters['features']
+            self.alg = parameters.get(self.ALGORITHM_PARAM, "Block")
+
+        if self.alg = "Block":
+            self.transpiler_utils.add_import("from recordlinkage.index import Block")
+
+        self.treatment()
+
+    def treatment(self):
+            if len(self.features) == 0:
+                raise ValueError(
+                    _("Parameter '{}' must be x>0 for task {}").format(
+                        self.ATTRIBUTES_PARAM, self.__class__))
