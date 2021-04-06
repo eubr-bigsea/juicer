3fc46b6fb167d43349125abb76803fd777b99895
eric <ericstefano12@gmail.com>
2021-04-06 12:23:24 -0300

    parameterizing clustering tests

--- tests/scikit_learn/clustering/test_agglomerative_clustering.py
+++ tests/scikit_learn/clustering/test_agglomerative_clustering.py
@@ -1,10 +1,34 @@
-from tests.scikit_learn import util
+import pytest
+from sklearn.cluster import AgglomerativeClustering as AGC
+
 from juicer.scikit_learn.clustering_operation import AgglomerativeModelOperation, \
     AgglomerativeClusteringOperation
-from sklearn.cluster import AgglomerativeClustering
+from tests.scikit_learn import util
 from tests.scikit_learn.util import get_X_train_data
-import pytest
-import pandas as pd
+
+
+@pytest.fixture
+def get_columns():
+    return ['sepallength', 'sepalwidth', 'petallength', 'petalwidth']
+
+
+@pytest.fixture
+def get_df(get_columns):
+    return util.iris(get_columns)
+
+
+@pytest.fixture
+def get_arguments(get_columns):
+    return {
+        'parameters': {'attributes': get_columns,
+                       'multiplicity': {'input data': 0}},
+        'named_inputs': {
+            'input data': 'df',
+        },
+        'named_outputs': {
+            'output data': 'out'
+        }
+    }
 
 
 # pd.set_option('display.max_rows', None)
@@ -16,161 +40,75 @@ import pandas as pd
 #
 #
 # # # # # # # # # # Success # # # # # # # # # #
-def test_agglomerative_clustering_success():
-    df = util.iris(['petalwidth', 'petallength'], size=30)
-    test_df = df.copy()
+@pytest.mark.parametrize(("operation_par", "algorithm_par"), [
+    ({}, {}),
 
-    arguments = {
-        'parameters': {'attributes': ['petalwidth', 'petallength'],
-                       'multiplicity': {'input data': 0}},
-        'named_inputs': {
-            'input data': 'df',
-        },
-        'named_outputs': {
-            'output data': 'out'
-        },
-    }
+    ({'number_of_clusters': 5}, {"n_clusters": 5}),
+
+    ({'linkage': 'single', 'affinity': 'l1'},
+     {"linkage": 'single', "affinity": 'l1'}),
+
+], ids=['default_params', 'number_of_clusters_param',
+        'affinity_and_linkage_params'])
+def test_agglomerative_clustering_params_success(get_columns, get_arguments,
+                                                 get_df, operation_par,
+                                                 algorithm_par):
+    test_df = get_df
+    arguments = get_arguments
+
+    arguments['parameters'].update(operation_par)
+
     arguments = util.add_minimum_ml_args(arguments)
     instance = AgglomerativeModelOperation(**arguments)
-    result = util.execute(util.get_complete_code(instance), {'df': df})
-    X = get_X_train_data(test_df, ['petalwidth', 'petallength'])
-    agg = AgglomerativeClustering(n_clusters=2,
-                                  linkage='ward', affinity='euclidean')
-    test_df['prediction'] = agg.fit_predict(X)
+    result = util.execute(util.get_complete_code(instance), {'df': get_df})
+    x_train = get_X_train_data(test_df, get_columns)
+    agg = AGC(n_clusters=2, linkage='ward', affinity='euclidean')
+
+    for key, value in algorithm_par.items():
+        setattr(agg, key, value)
+
+    test_df['prediction'] = agg.fit_predict(x_train)
     assert result['out'].equals(test_df)
     assert str(result['model_task_1']) == str(agg)
 
 
-def test_agglomerative_clustering_alias_param_success():
-    df = util.iris(['petalwidth', 'petallength'], size=30)
-    arguments = {
-        'parameters': {'attributes': ['petalwidth', 'petallength'],
-                       'multiplicity': {'input data': 0},
-                       'alias': 'success'},
-        'named_inputs': {
-            'input data': 'df',
-        },
-        'named_outputs': {
-            'output data': 'out'
-        }
-    }
+def test_agglomerative_clustering_alias_param_success(get_df, get_arguments,
+                                                      get_columns):
+    arguments = get_arguments
+    arguments['parameters'].update({'alias': 'success'})
     arguments = util.add_minimum_ml_args(arguments)
     instance = AgglomerativeModelOperation(**arguments)
-    result = util.execute(util.get_complete_code(instance), {'df': df})
-    assert result['out'].columns[2] == 'success'
+    result = util.execute(util.get_complete_code(instance), {'df': get_df})
+    assert result['out'].columns[len(get_columns)] == 'success'
 
 
-def test_agglomerative_clustering_number_of_clusters_param_success():
-    df = util.iris(['petalwidth', 'petallength'], size=30)
-    test_df = df.copy()
+@pytest.mark.parametrize(('selector', 'drop'), [
+    ("named_outputs", "output data"),
 
-    arguments = {
-        'parameters': {'attributes': ['petalwidth', 'petallength'],
-                       'multiplicity': {'input data': 0},
-                       'number_of_clusters': 5},
-        'named_inputs': {
-            'input data': 'df',
-        },
-        'named_outputs': {
-            'output data': 'out'
-        }
-    }
-    arguments = util.add_minimum_ml_args(arguments)
-    instance = AgglomerativeModelOperation(**arguments)
-    result = util.execute(util.get_complete_code(instance), {'df': df})
+    ("named_inputs", "input data")
 
-    X = get_X_train_data(test_df, ['petalwidth', 'petallength'])
-    agg = AgglomerativeClustering(n_clusters=5,
-                                  linkage='ward', affinity='euclidean')
-    test_df['prediction'] = agg.fit_predict(X)
-    assert result['out'].equals(test_df)
-    assert str(result['model_task_1']) == str(agg)
-
-
-def test_agglomerative_clustering_affinity_and_linkage_params_success():
-    df = util.iris(['petalwidth', 'petallength'], size=30)
-    test_df = df.copy()
-
-    arguments = {
-        'parameters': {'attributes': ['petalwidth', 'petallength'],
-                       'multiplicity': {'input data': 0},
-                       'linkage': 'single',
-                       'affinity': 'l1'},
-        'named_inputs': {
-            'input data': 'df',
-        },
-        'named_outputs': {
-            'output data': 'out'
-        }
-    }
-    arguments = util.add_minimum_ml_args(arguments)
-    instance = AgglomerativeModelOperation(**arguments)
-    result = util.execute(util.get_complete_code(instance), {'df': df})
-    X = get_X_train_data(test_df, ['petalwidth', 'petallength'])
-    agg = AgglomerativeClustering(n_clusters=2,
-                                  linkage='single', affinity='l1')
-    test_df['prediction'] = agg.fit_predict(X)
-    assert result['out'].equals(test_df)
-    assert str(result['model_task_1']) == str(agg)
-
-
-def test_agglomerative_clustering_no_output_implies_no_code_success():
-    arguments = {
-        'parameters': {'attributes': ['petalwidth', 'petallength'],
-                       'multiplicity': {'input data': 0}},
-        'named_inputs': {
-            'input data': 'df',
-        },
-        'named_outputs': {
-        }
-    }
-    instance = AgglomerativeModelOperation(**arguments)
-    assert instance.generate_code() is None
-
-
-def test_agglomerative_clustering_missing_input_implies_no_code_success():
-    arguments = {
-        'parameters': {'attributes': ['petalwidth', 'petallength'],
-                       'multiplicity': {'input data': 0}},
-        'named_inputs': {
-        },
-        'named_outputs': {
-            'output data': 'out'
-        }
-    }
+], ids=["missing_output", "missing_input"])
+def test_agglomerative_clustering_no_code_success(get_arguments, selector, drop):
+    arguments = get_arguments
+    arguments[selector].pop(drop)
     instance = AgglomerativeModelOperation(**arguments)
     assert instance.generate_code() is None
 
 
 # # # # # # # # # # Fail # # # # # # # # # #
-def test_agglomerative_clustering_missing_attributes_param_fail():
-    arguments = {
-        'parameters': {'multiplicity': {'input data': 0}},
-        'named_inputs': {
-            'input data': 'df',
-        },
-        'named_outputs': {
-            'output data': 'out'
-        }
-    }
+def test_agglomerative_clustering_missing_attributes_param_fail(get_arguments):
+    arguments = get_arguments
+    arguments['parameters'].pop("attributes")
     with pytest.raises(ValueError) as val_err:
         AgglomerativeModelOperation(**arguments)
     assert f"Parameter 'attributes' must be informed for task" \
            f" {AgglomerativeClusteringOperation}" in str(val_err)
 
 
-def test_agglomerative_clustering_invalid_number_of_clusters_param_fail():
-    arguments = {
-        'parameters': {'attributes': ['petalwidth', 'petallength'],
-                       'multiplicity': {'input data': 0},
-                       'number_of_clusters': -1},
-        'named_inputs': {
-            'input data': 'df',
-        },
-        'named_outputs': {
-            'output data': 'out'
-        }
-    }
+def test_agglomerative_clustering_invalid_number_of_clusters_param_fail(
+        get_arguments):
+    arguments = get_arguments
+    arguments['parameters'].update({'number_of_clusters': -1})
     with pytest.raises(ValueError) as val_err:
         AgglomerativeModelOperation(**arguments)
     assert f"Parameter 'number_of_clusters' must be x>0 for task" \
--- tests/scikit_learn/clustering/test_db_scan_clustering.py
+++ tests/scikit_learn/clustering/test_db_scan_clustering.py
@@ -1,203 +1,118 @@
-from tests.scikit_learn import util
+import pytest
+from sklearn.cluster import DBSCAN
+
 from juicer.scikit_learn.clustering_operation import \
     DBSCANClusteringModelOperation, DBSCANClusteringOperation, \
     ClusteringModelOperation
+from tests.scikit_learn import util
 from tests.scikit_learn.util import get_X_train_data
-from sklearn.cluster import DBSCAN
-import pytest
-import pandas as pd
 
 
 # pd.set_option('display.max_rows', None)
 # pd.set_option('display.max_columns', None)
 # pd.set_option('display.max_colwidth', None)
+@pytest.fixture
+def get_columns():
+    return ['sepallength', 'sepalwidth', 'petallength', 'petalwidth']
+
+
+@pytest.fixture
+def get_df(get_columns):
+    return util.iris(get_columns)
+
+
+@pytest.fixture
+def get_arguments(get_columns):
+    return {
+        'parameters': {'features': get_columns,
+                       'multiplicity': {'train input data': 0}},
+        'named_inputs': {
+            'train input data': 'df',
+        },
+        'named_outputs': {
+            'output data': 'out'
+        }
+    }
 
 
 # DBSCANClustering
 #
 #
 # # # # # # # # # # Success # # # # # # # # # #
-def test_db_scan_clustering_success():
-    df = util.iris(['sepallength', 'sepalwidth'], size=10)
-    test_df = df.copy()
+@pytest.mark.parametrize(("operation_par", "algorithm_par"), [
+    ({}, {}),
 
-    arguments = {
-        'parameters': {'features': ['sepallength', 'sepalwidth'],
-                       'multiplicity': {'train input data': 0}},
-        'named_inputs': {
-            'train input data': 'df',
-        },
-        'named_outputs': {
-            'output data': 'out'
-        }
-    }
+    ({'eps': 0.2}, {'eps': 0.2}),
+
+    ({'min_samples': 10}, {"min_samples": 10}),
+
+    ({'metric': 'manhattan'}, {"metric": 'manhattan'})
+
+], ids=["default_params", "eps_param", "min_samples_param", "metric_param"])
+def test_db_scan_clustering_params_success(get_df,
+                                           get_arguments,
+                                           get_columns,
+                                           operation_par,
+                                           algorithm_par):
+    test_df = get_df
+    arguments = get_arguments
+
+    arguments['parameters'].update(operation_par)
+
     arguments = util.add_minimum_ml_args(arguments)
     instance = DBSCANClusteringModelOperation(**arguments)
-    result = util.execute(util.get_complete_code(instance), {'df': df})
-    X_train = get_X_train_data(test_df, ['sepallength', 'sepalwidth'])
+    result = util.execute(util.get_complete_code(instance), {'df': get_df})
+    x_train = get_X_train_data(test_df,
+                               get_columns)
     dbscan = DBSCAN(eps=0.5, min_samples=5, metric='euclidean')
-    test_df['prediction'] = dbscan.fit_predict(X_train)
+
+    for key, value in algorithm_par.items():
+        setattr(dbscan, key, value)
+
+    test_df['prediction'] = dbscan.fit_predict(x_train)
     assert result['out'].equals(test_df)
     assert str(result['model_task_1']) == str(dbscan)
 
 
-def test_db_scan_clustering_eps_param_success():
-    df = util.iris(['sepallength', 'sepalwidth'], size=10)
-    test_df = df.copy()
+def test_db_scan_clustering_prediction_param_success(get_df, get_arguments,
+                                                     get_columns):
+    arguments = get_arguments
+    arguments['parameters'].update({'prediction': 'success'})
 
-    arguments = {
-        'parameters': {'features': ['sepallength', 'sepalwidth'],
-                       'multiplicity': {'train input data': 0},
-                       'eps': 0.2},
-        'named_inputs': {
-            'train input data': 'df',
-        },
-        'named_outputs': {
-            'output data': 'out'
-        }
-    }
     arguments = util.add_minimum_ml_args(arguments)
     instance = DBSCANClusteringModelOperation(**arguments)
-    result = util.execute(util.get_complete_code(instance), {'df': df})
-    X_train = get_X_train_data(test_df, ['sepallength', 'sepalwidth'])
-    dbscan = DBSCAN(eps=0.2, min_samples=5, metric='euclidean')
-    test_df['prediction'] = dbscan.fit_predict(X_train)
-    assert result['out'].equals(test_df)
-    assert str(result['model_task_1']) == str(dbscan)
+    result = util.execute(util.get_complete_code(instance), {'df': get_df})
+    assert result['out'].columns[len(get_columns)] == 'success'
 
 
-def test_db_scan_clustering_min_samples_param_success():
-    df = util.iris(['sepallength', 'sepalwidth'], size=10)
-    test_df = df.copy()
+@pytest.mark.parametrize(('selector', 'drop'), [
+    ("named_outputs", "output data"),
 
-    arguments = {
-        'parameters': {'features': ['sepallength', 'sepalwidth'],
-                       'multiplicity': {'train input data': 0},
-                       'min_samples': 10},
-        'named_inputs': {
-            'train input data': 'df',
-        },
-        'named_outputs': {
-            'output data': 'out'
-        }
-    }
-    arguments = util.add_minimum_ml_args(arguments)
-    instance = DBSCANClusteringModelOperation(**arguments)
-    result = util.execute(util.get_complete_code(instance), {'df': df})
-    X_train = get_X_train_data(test_df, ['sepallength', 'sepalwidth'])
-    dbscan = DBSCAN(eps=0.5, min_samples=10, metric='euclidean')
-    test_df['prediction'] = dbscan.fit_predict(X_train)
-    assert result['out'].equals(test_df)
-    assert str(result['model_task_1']) == str(dbscan)
+    ("named_inputs", "train input data")
 
-
-def test_db_scan_clustering_prediction_param_success():
-    df = util.iris(['sepallength', 'sepalwidth'], size=10)
-    arguments = {
-        'parameters': {'features': ['sepallength', 'sepalwidth'],
-                       'multiplicity': {'train input data': 0},
-                       'prediction': 'success'},
-        'named_inputs': {
-            'train input data': 'df',
-        },
-        'named_outputs': {
-            'output data': 'out'
-        }
-    }
-    arguments = util.add_minimum_ml_args(arguments)
-    instance = DBSCANClusteringModelOperation(**arguments)
-    result = util.execute(util.get_complete_code(instance), {'df': df})
-    assert result['out'].columns[2] == 'success'
-
-
-def test_db_scan_clustering_metric_param_success():
-    df = util.iris(['sepallength', 'sepalwidth'], size=10)
-    test_df = df.copy()
-
-    arguments = {
-        'parameters': {'features': ['sepallength', 'sepalwidth'],
-                       'multiplicity': {'train input data': 0},
-                       'metric': 'manhattan'},
-        'named_inputs': {
-            'train input data': 'df',
-        },
-        'named_outputs': {
-            'output data': 'out'
-        }
-    }
-    arguments = util.add_minimum_ml_args(arguments)
-    instance = DBSCANClusteringModelOperation(**arguments)
-    result = util.execute(util.get_complete_code(instance), {'df': df})
-    X_train = get_X_train_data(test_df, ['sepallength', 'sepalwidth'])
-    dbscan = DBSCAN(eps=0.5, min_samples=5, metric='manhattan')
-    test_df['prediction'] = dbscan.fit_predict(X_train)
-    assert result['out'].equals(test_df)
-    assert str(result['model_task_1']) == str(dbscan)
-
-
-def test_db_scan_clustering_no_output_implies_no_code_success():
-    arguments = {
-        'parameters': {'features': ['sepallength', 'sepalwidth'],
-                       'multiplicity': {'train input data': 0}},
-        'named_inputs': {
-            'train input data': 'df',
-        },
-        'named_outputs': {
-        }
-    }
-    instance = DBSCANClusteringModelOperation(**arguments)
-    assert instance.generate_code() is None
-
-
-def test_db_scan_clustering_missing_input_implies_no_code_success():
-    arguments = {
-        'parameters': {'features': ['sepallength', 'sepalwidth'],
-                       'multiplicity': {'train input data': 0}},
-        'named_inputs': {
-        },
-        'named_outputs': {
-            'output data': 'out'
-        }
-    }
+], ids=["missing_output", "missing_input"])
+def test_db_scan_clustering_no_code_success(get_arguments, selector, drop):
+    arguments = get_arguments
+    arguments[selector].pop(drop)
     instance = DBSCANClusteringModelOperation(**arguments)
     assert instance.generate_code() is None
 
 
 # # # # # # # # # # Fail # # # # # # # # # #
-def test_db_scan_clustering_missing_features_param_fail():
-    arguments = {
-        'parameters': {'multiplicity': {'train input data': 0}},
-        'named_inputs': {
-            'train input data': 'df',
-        },
-        'named_outputs': {
-            'output data': 'out'
-        }
-    }
+def test_db_scan_clustering_missing_features_param_fail(get_arguments):
+    arguments = get_arguments
+    arguments['parameters'].pop("features")
     with pytest.raises(ValueError) as val_err:
         DBSCANClusteringModelOperation(**arguments)
     assert f"Parameter 'features' must be informed for task" \
            f" {ClusteringModelOperation}" in str(val_err.value)
 
 
-def test_db_scan_clustering_invalid_eps_and_min_samples_params_fail():
-    pars = [
-        'min_samples',
-        'eps'
-    ]
-    for val in pars:
-        arguments = {
-            'parameters': {f'features': ['sepallength', 'sepalwidth'],
-                           'multiplicity': {'train input data': 0},
-                           val: -1},
-            'named_inputs': {
-                'train input data': 'df',
-            },
-            'named_outputs': {
-                'output data': 'out'
-            }
-        }
-        with pytest.raises(ValueError) as val_err:
-            DBSCANClusteringModelOperation(**arguments)
-        assert f"Parameter '{val}' must be x>0 for task" \
-               f" {DBSCANClusteringOperation}" in str(val_err.value)
+@pytest.mark.parametrize('par', ['min_samples', 'eps'])
+def test_db_scan_clustering_invalid_params_fail(get_arguments, par):
+    arguments = get_arguments
+    arguments['parameters'].update({par: -1})
+    with pytest.raises(ValueError) as val_err:
+        DBSCANClusteringModelOperation(**arguments)
+    assert f"Parameter '{par}' must be x>0 for task" \
+           f" {DBSCANClusteringOperation}" in str(val_err.value)
