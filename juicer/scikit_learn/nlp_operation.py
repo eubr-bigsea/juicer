79df41d7c449ef1cea42f549a2d1513fccf20ed1
luiz <luiz10hdsg@gmail.com>
2022-05-26 23:00:49 -0300

    add antonyms operation

--- juicer/scikit_learn/nlp_operation.py
+++ juicer/scikit_learn/nlp_operation.py
@@ -166,4 +166,60 @@ class SynonymsOperation(Operation):
                 input = self.input,
                 out=self.output)
         return dedent(code)
-  
+
+class AntonymsOperation(Operation):
+    """This operation generate antonyms of a word.
+    """
+    ATTRIBUTES_PARAM = 'attributes'
+    ALIAS_PARAM = 'alias'
+
+    def __init__(self, parameters, named_inputs, named_outputs):
+        Operation.__init__(self, parameters, named_inputs, named_outputs)
+
+
+        self.attributes = parameters.get(self.ATTRIBUTES_PARAM, []) or []
+        if not self.attributes:
+            raise ValueError(
+                _("Parameter '{}' must be informed for task {}")
+                .format(self.ATTRIBUTES_PARAM, self.__class__))
+
+        self.alias = [ alias.strip() for alias in parameters.get(self.ALIAS_PARAM, '').split(',')]
+
+        # Adjust alias in order to have the same number of aliases as attributes
+        # by filling missing alias with the attribute name suffixed by _pdf.
+        self.alias = [x[1] or '{}_antonyms'.format(x[0]) for x
+                in zip_longest(self.attributes, self.alias[:len(self.attributes)])]
+
+        self.output = self.named_outputs.get(
+                'output data', 'output_data_{}'.format(self.order))
+
+        self.input = self.named_inputs.get(
+                'input data', 'input_data_{}'.format(self.order))
+        self.has_code = any([len(named_outputs) > 0, self.contains_results()])
+
+    def generate_code(self):
+        """Generate code."""
+        code = """
+        import nltk
+        nltk.download('wordnet')
+        from nltk.corpus import wordnet
+        def antonyms(word):
+            antonyms = []
+            synonyms = []
+            aux = []
+            for syn in wordnet.synsets(word):
+                for l in syn.lemmas():
+                     synonyms.append(l.name())
+                     if l.antonyms():
+                        antonyms.append(l.antonyms()[0].name())
+            aux = antonyms
+            return aux
+        {out} = {input}
+        alias = {alias}
+        for i, attr in enumerate({attributes}):
+            {out}[alias[i]] = {input}[attr].apply(antonyms)
+        """.format(attributes=self.attributes,
+                alias = self.alias,
+                input = self.input,
+                out=self.output)
+        return dedent(code)
