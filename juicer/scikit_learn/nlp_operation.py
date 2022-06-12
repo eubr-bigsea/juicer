d04adce942b8d1d11ad4f2de2a9d8a556c337c59
ealucas <ealucas@github.com>
2022-06-12 19:57:04 -0300

    add word segmentation

--- juicer/scikit_learn/nlp_operation.py
+++ juicer/scikit_learn/nlp_operation.py
@@ -194,3 +194,49 @@ class PosTaggingOperation(Operation):
                 input = self.input,
                 out=self.output)
         return dedent(code)
+
+class WordSegmentationOperation(Operation):
+    """Word Segmentation is the process of dividing a string into its word components.
+    """
+    ATTRIBUTES_PARAM = 'attributes'
+    ALIAS_PARAM = 'alias'
+
+    def __init__(self, parameters, named_inputs, named_outputs):
+        Operation.__init__(self, parameters, named_inputs, named_outputs)
+        
+
+        self.attributes = parameters.get(self.ATTRIBUTES_PARAM, []) or []
+        if not self.attributes:
+            raise ValueError(
+                _("Parameter '{}' must be informed for task {}")
+                .format(self.ATTRIBUTES_PARAM, self.__class__))
+
+        self.alias = [ alias.strip() for alias in parameters.get(self.ALIAS_PARAM, '').split(',')] 
+
+        # Adjust alias in order to have the same number of aliases as attributes 
+        # by filling missing alias with the attribute name suffixed by _pdf.
+        self.alias = [x[1] or '{}_segmentedwords'.format(x[0]) for x 
+                in zip_longest(self.attributes, self.alias[:len(self.attributes)])] 
+
+        self.output = self.named_outputs.get(
+                'output data', 'output_data_{}'.format(self.order))
+
+        self.input = self.named_inputs.get(
+                'input data', 'input_data_{}'.format(self.order))
+        self.has_code = any([len(named_outputs) > 0, self.contains_results()]) 
+
+    def generate_code(self):
+        """Generate code."""
+        code = """
+        load()
+        def segmentWord(sentence):
+            return segment(sentence)
+        {out} = {input}
+        alias = {alias}
+        for i, attr in enumerate({attributes}):
+            {out}[alias[i]] = {input}[attr].apply(segmentWord)
+        """.format(attributes=self.attributes,
+                alias = self.alias,
+                input = self.input,
+                out=self.output)
+        return dedent(code)
