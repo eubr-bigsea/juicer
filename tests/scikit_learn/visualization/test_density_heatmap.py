7a670a98a1b20899ca356d1e8e058a6baed4025b
Luiz20hdsg <luiz10hdsg@gmail.com>
2023-06-17 03:39:16 +0000

    update

--- tests/scikit_learn/visualization/test_density_heatmap.py
+++ tests/scikit_learn/visualization/test_density_heatmap.py
@@ -10,6 +10,8 @@ import pytest
 import pandas as pd
 import polars as pl
 import plotly.express as px
+import plotly.colors
+
 
 
 # DensityHeatmap
@@ -67,34 +69,7 @@ def test_test_dentity_heatmap():
                         }
 
     instance = VisualizationOperation(**arguments)
-    # Gerar codigo de todo o template ??
-    # Como expecificar a execução de um grafico expecifico no template ??
-    # JSON ??
-
-    # Gerar código do template
     
-    '''instance.template = {%- if plot_code == 'heatmap' %}
-    fig = px.density_heatmap(pandas_df, 
-                                 x='dim_0', 
-                                 y='aggr_0', 
-                                 marginal_x="box", 
-                                 marginal_y="violin",
-                                 title='{{op.title}}',
-                                 labels=labels,
-                                 color_continuous_scale={{op.color_scale}})
-    {% endif %}'''
-
-    '''
-    
-    # Definir o código específico para o heatmap
-    instance.template_vars['plot_code'] = 'heatmap'
-    
-    # Gerar código a partir do template
-    code = instance.generate_code()
-    
-    # Extrair o trecho do código do heatmap
-    heatmap_code = instance.template.extract_part(code, 'fig =', '})')
-    '''
     def emit_event(*args, **kwargs):
         print(args, kwargs)
 
@@ -107,22 +82,12 @@ def test_test_dentity_heatmap():
     generated_chart = result.get('d')
     import pdb;pdb.set_trace()
 
-    #print(result.keys())
-    atributos = result.keys()
-    #print(result['out'])
-    #print(type(generated_chart))
-    #chaves2 = generated_chart.keys()
-    #print(chaves2)
-    #print(type(result))
-    #chaves = result.keys()
-    #print(chaves)
-    #print(result.items)
-    atributo = result.get('d')
-    print(atributo)  # Saída: valor2
-    print(type(atributo))
-    print(atributo.keys())
-    data = atributo['data']
-    layout = atributo['layout']
+    #atributos = result.keys()
+    #atributo = result.get('d')
+  
+    data = generated_chart['data']
+    layout = generated_chart['layout']
+
     print(data)
     coloraxis = data[0]['y']
     print(coloraxis)
@@ -131,49 +96,43 @@ def test_test_dentity_heatmap():
     print(scatter)
     scatter2 = layout['template']['data']
     print(scatter2)
+
     ## Rever o código ##
     # Codigo de teste
-    df_select = df.select("sepallength")
-    df_select_result = df_select.lazy().select("sepallength").collect()
-    df_select1 = df.select("sepalwidth")
-    df_select_result1 = df_select1.lazy().select("sepalwidth").collect()
-    print(df_select_result)
-    print(df_select_result1)
+    '''
+    df_select = df.select("petalwidth")
+    df_select_result = df_select.lazy().select("petalwidth").collect()
+    df_select1 = df.select("petallength")
+    df_select_result1 = df_select1.lazy().select("petallength").collect()
     df_select_result_pd = df_select_result.to_pandas()
     df_select_result1_pd = df_select_result1.to_pandas()
-    #print(df_select)
-   #fig = px.density_heatmap(df, x=df_select_result, y=df_select_result1, marginal_x="box", marginal_y="violin")
-    #df_pandas = df.to_pandas()
+   '''
     df_pol = df.collect()
     df_pandas = df_pol.to_pandas()
-    fig = px.density_heatmap(df_pandas, x="sepallength", y="sepalwidth", marginal_x="box", marginal_y="violin")
+
+    # Definir uma escala de cores personalizada
+    custom_colors = plotly.colors.sequential.Viridis
+
+    # Gerar o gráfico com a escala de cores personalizada
+    fig = px.density_heatmap(df_pandas, x="sepallength", y="sepalwidth", color_continuous_scale=custom_colors, marginal_x="box", marginal_y="violin", title="test")
 
     #fig = px.density_heatmap(df, x=df_select_result, y=df_select_result1, marginal_x="box", marginal_y="violin")
 
 
     # Converter em JSON
     fig_json = fig.to_json()
-    dicionario = json.loads(fig_json)
-    print(dicionario)
-    print(dicionario.keys())
-    print(type(fig_json))
+    generated_chart_vis = json.loads(fig_json)
+    print(generated_chart_vis)
+    print(generated_chart_vis.keys())
+    data1 = generated_chart['data']
+    layout1 = generated_chart['layout']
+
+    print(data1)
+    coloraxis = data1[0]['y']
+    print(coloraxis)
+    print(layout)
+    scatter = layout1.keys()
+    print(scatter)
+    scatter2 = layout1['template']['data']
+    print(scatter2)
    
-   
-    '''
-    # Tentar carregar o JSON como um objeto Python
-    try:
-        json_obj = json.loads(fig_json)
-    except json.JSONDecodeError:
-        assert False, "Falha ao carregar o JSON gerado"
-
-    # Verificar se o JSON carregado possui as informações esperadas
-    assert "data" in json_obj
-    assert "layout" in json_obj
-    
-    # Verificar se o JSON gerado é igual ao JSON da vizualização Lemonade
-    # teste_out
-    # assert result['out'].equals(test_df)
-
-    assert fig_json == fig.to_json()
-
-'''
