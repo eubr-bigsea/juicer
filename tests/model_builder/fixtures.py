1c3e30196d21e6717c7969e0995ff3585b9fffdd
Walter dos Santos Filho <waltersf@gmail.com>
2023-10-06 11:49:30 -0300

    Implementation (WIP)

--- tests/model_builder/fixtures.py
+++ tests/model_builder/fixtures.py
@@ -51,6 +51,21 @@ def mock_get_operations(*args):
         {'id': 2352, 'slug': 'features-reduction', 'ports': []},
         {'id': 2353, 'slug': 'grid', 'ports': []},
         {'id': 2354, 'slug': 'features', 'ports': []},
+        {'id': 2355, 'slug': 'k-means', 'ports': []},
+        {'id': 2356, 'slug': 'gaussian-mix', 'ports': []},
+        {'id': 2357, 'slug': 'decision-tree-classifier', 'ports': []},
+        {'id': 2358, 'slug': 'gbt-classifier', 'ports': []},
+        {'id': 2359, 'slug': 'naive-bayes', 'ports': []},
+        {'id': 2360, 'slug': 'perceptron', 'ports': []},
+        {'id': 2361, 'slug': 'random-forest-classifier', 'ports': []},
+        {'id': 2362, 'slug': 'logistic-regression', 'ports': []},
+        {'id': 2363, 'slug': 'svm', 'ports': []},
+        {'id': 2364, 'slug': 'linear-regression', 'ports': []},
+        {'id': 2365, 'slug': 'isotonic-regression', 'ports': []},
+        {'id': 2366, 'slug': 'gbt-regressor', 'ports': []},
+        {'id': 2367, 'slug': 'random-forest-regressor', 'ports': []},
+        {'id': 2368, 'slug': 'generalized-linear-regressor', 'ports': []},
+        {'id': 2369, 'slug': 'decision-tree-regressor', 'ports': []}
     ]
 
 
@@ -63,28 +78,29 @@ def sample_workflow() -> dict:
         return json.load(f)
 
 @pytest.fixture(scope='function')
-@patch('juicer.service.limonero_service.get_data_source_info')
-@patch('juicer.workflow.workflow.Workflow._get_operations')
-def builder_params(mocked_ops: MagicMock, limonero: MagicMock, 
-        sample_workflow: dict):
-    
-    mocked_ops.side_effect = mock_get_operations
-    limonero.side_effect = mock_get_datasource
+def builder_params(sample_workflow: dict):
+    """
+    This fixture mocks methods used to interact with other Lemonade
+    services (e.g. Limonero and Tahiti). It also returns an object
+    organized according to the Model Builder template.
+    """
+    with patch('juicer.workflow.workflow.Workflow._get_operations', 
+        return_value=mock_get_operations()):
+        with patch('juicer.service.limonero_service.get_data_source_info',
+            return_value=mock_get_datasource()):
 
-    loader = Workflow(sample_workflow, config, lang='en')
+            loader = Workflow(sample_workflow, config, lang='en')
+            instances = loader.workflow['tasks']
 
-    instances = loader.workflow['tasks']
+            minion = MetaMinion(None, config=config, workflow_id=sample_workflow['id'],
+                                app_id=sample_workflow['id'])
 
-    minion = MetaMinion(None, config=config, workflow_id=sample_workflow['id'],
-                        app_id=sample_workflow['id'])
+            job_id = 1
+            opt = GenerateCodeParams(loader.graph, job_id, None, {}, 
+                ports={}, state={}, task_hash=hashlib.sha1(), 
+                workflow=loader.workflow,
+                tasks_ids=list(loader.graph.nodes.keys()))
+            instances, _ = minion.transpiler.get_instances(opt)
 
-    job_id = 1
-    opt = GenerateCodeParams(loader.graph, job_id, None, {}, 
-        ports={}, state={}, task_hash=hashlib.sha1(), 
-        workflow=loader.workflow,
-        tasks_ids=list(loader.graph.nodes.keys()))
-    instances, _ = minion.transpiler.get_instances(opt)
-
-    builder_params = minion.transpiler.prepare_model_builder_parameters(ops=instances.values())
-    return builder_params
-   
+            builder_params = minion.transpiler.prepare_model_builder_parameters(ops=instances.values())
+            yield builder_params
